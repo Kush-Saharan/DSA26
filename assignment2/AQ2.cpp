c6f1d9e5c8cb41dabfb9aa8c235e3b66bf4f6004
Kush-Saharan <kush.saharan29@gmail.com>
2026-01-30 00:58:55 +0530

    wip: partial solution of assignment 2

---
+++ assignment2/AQ2.cpp
@@ -0,0 +1,45 @@
+/*
+Given the arrival and departure times of all trains reaching a railway station on the same day, write a program to
+determine the minimum number of platforms required so that no train has to wait for a platform. For each train, the
+arrival time is always different from its departure time, but the arrival time of one train may be equal to the departure
+time of another train. At any given instant, a single platform cannot be used simultaneously for the departure of one
+train and the arrival of another train; therefore, in such cases, separate platforms must be allocated. [Minimum
+Platforms | Practice Problems]
+Input:
+Train = [T1, T2, T3, T4, T5]
+AT = [09:00, 09:10, 09:20, 11:00, 11:20]
+DT = [09:40, 12:00, 09:50, 11:30, 11:40]
+Output:
+Minimum number of platforms required = 3
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> AT={900, 910, 920, 1100, 1120};
+    vector<int> DT={940, 1200, 950, 1130, 1140};
+
+    sort(AT.begin(), AT.end());
+    sort(DT.begin(), DT.end());
+
+    int i=0, j=0;
+    int platforms=0, maxPlatforms=0;
+
+    while (i<AT.size() && j<DT.size()) {
+        if(AT[i]<=DT[j]) {
+            platforms++;
+            maxPlatforms=max(maxPlatforms, platforms);
+            i++;
+        }
+        else{
+            platforms--;
+            j++;
+        }
+    }
+    cout<<"Minimum number of platforms required = " << maxPlatforms << endl;
+
+    return 0;
+}
